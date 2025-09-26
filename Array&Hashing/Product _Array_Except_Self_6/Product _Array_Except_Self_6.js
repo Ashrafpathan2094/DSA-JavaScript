30f323080df6b93688f35afb7131f456ee33de18
Ashraf I Khan <minazpathan2094@gmail.com>
2025-09-26 15:32:54 +0530

    added self product with division

--- Array&Hashing/Product _Array_Except_Self_6/Product _Array_Except_Self_6.js
+++ Array&Hashing/Product _Array_Except_Self_6/Product _Array_Except_Self_6.js
@@ -1,7 +1,23 @@
 var productExceptSelf = function (nums) {
-  const allCount = nums.reduce((acc, curr) => acc * curr, 1);
-  console.log("🚀 ~ productExceptSelf ~ allCount:", allCount);
-  return nums.map((num) => allCount / num);
+  const zeroCount = nums.filter((num) => num === 0).length;
+
+  // If there are more than one zeros, all products will be 0
+  if (zeroCount > 1) {
+    return new Array(nums.length).fill(0);
+  }
+
+  // If there's exactly one zero
+  if (zeroCount === 1) {
+    const productWithoutZero = nums.reduce(
+      (acc, curr) => (curr !== 0 ? acc * curr : acc),
+      1
+    );
+    return nums.map((num) => (num === 0 ? productWithoutZero : 0));
+  }
+
+  // If there are no zeros
+  const allProduct = nums.reduce((acc, curr) => acc * curr, 1);
+  return nums.map((num) => allProduct / num);
 };
 
 console.log(productExceptSelf([1, 2, 3, 4])); // [24,12,8,6]
