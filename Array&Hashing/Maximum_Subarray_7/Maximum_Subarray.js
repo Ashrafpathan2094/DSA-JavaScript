1883dce9e783adb6c517b4dbedbc43e5d2e24fe3
Ashraf I Khan <minazpathan2094@gmail.com>
2025-10-01 15:45:52 +0530

    added maximum subarray using Kadane's Algorithm

---
+++ Array&Hashing/Maximum_Subarray_7/Maximum_Subarray.js
@@ -0,0 +1,14 @@
+var maxSubArray = function (nums) {
+  let maxSum = nums[0];
+  let currentSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (currentSum < 0) {
+      currentSum = 0;
+    }
+    currentSum += nums[i];
+    maxSum = Math.max(maxSum, currentSum);
+  }
+  return maxSum;
+};
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
