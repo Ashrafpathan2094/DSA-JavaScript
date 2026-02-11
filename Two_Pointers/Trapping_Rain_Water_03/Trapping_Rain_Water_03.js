55f640f999845558a723130d0cc1663646fa52e3
Ashraf I Khan <minazpathan2094@gmail.com>
2026-02-11 18:43:06 +0530

    trapping rain water done

--- Two_Pointers/Trapping_Rain_Water_03/Trapping_Rain_Water_03.js
+++ Two_Pointers/Trapping_Rain_Water_03/Trapping_Rain_Water_03.js
@@ -1,35 +1,28 @@
-var trap = function (height) {
-  if (height.length === 0) return 0;
-  if (height.length === 3) {
-    const min = Math.min(height[0], height[2]);
-    return min - height[1] > 0 ? min - height[1] : 0;
-  }
-
-  let max = 0;
-  let i = 0;
-  while (i <= height.length) {
-    if (height[i] > height[i + 1]) {
-      let j = i + 2;
-      while (height[i] > height[j] && j <= height.length) {
-        j++;
-      }
-      if (height[i] <= height[j] || j != height.length) {
-        const min = Math.min(height[i], height[j]);
-        max = max + getNewMaxVal(max, min, height.slice(i + 1, j));
-      }
-      i = j;
-      continue;
+const trap = (height) => {
+  let water = 0;
+  let leftIndex = 0;
+  let rightIndex = height.length - 1;
+  let maxLeft = height[leftIndex];
+  let maxRight = height[rightIndex];
+  while (leftIndex != rightIndex) {
+    if (height[leftIndex] > height[rightIndex]) {
+      rightIndex--;
+      maxRight = Math.max(maxRight, height[rightIndex]);
+      water = water + maxRight - height[rightIndex];
     } else {
-      i++;
-      continue;
+      leftIndex++;
+      maxLeft = Math.max(maxLeft, height[leftIndex]);
+      water = water + maxLeft - height[leftIndex];
     }
   }
-  return max;
-};
-const getNewMaxVal = (max, min, array) => {
-  return array.reduce((acc, item) => acc + (min - item), max);
+  return water;
 };
 
-// console.log("trap()", trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
-// console.log("trap()", trap([4, 2, 0, 3, 2, 5]));
+// console.log("trap()", trap([5, 4, 1, 2]));
+
+// const min = Math.min(height[leftIndex], height[j]);
+// max = max + getNewMaxVal(max, min, height.slice(i + 1, j));
+// console.log("height[j]", height[j]);
+console.log("trap()", trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+console.log("trap()", trap([4, 2, 0, 3, 2, 5]));
 console.log("trap()", trap([4, 2, 3]));
